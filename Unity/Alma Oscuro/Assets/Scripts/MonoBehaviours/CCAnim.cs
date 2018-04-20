74043d96fb99666c002f2b7ec8cac1f5fd1298ca
Fritz Dittmer <fdittmer9762@gmail.com>
2018-04-20 14:33:27 -0600

    PlayerMovementRough
    Added PlayerAnimation and Animator
    Added StateMachineBehaviours
    Created Movement MB
    Debugging: Issue Found
    -Issues with the way CC.move is called right now. issues called with overlap Between Animaitons

---
+++ Unity/Alma Oscuro/Assets/Scripts/MonoBehaviours/CCAnim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCAnim : MonoBehaviour {
+
+	private CharacterController CC;
+
+	private Animator Anim;
+
+	private Vector3 TempPos;
+
+	public float playerSpeed;
+
+	void Start(){
+		CC = GetComponent<CharacterController> ();
+		Anim = GetComponentInChildren<Animator> ();
+	}
+
+	void Update(){
+		MoveCharacter ();
+	}
+
+	public void MoveCharacter(){
+		TempPos = new Vector3(Anim.GetFloat("TempX"),Anim.GetFloat("TempY"),Anim.GetFloat("TempZ"));
+		CC.Move (TempPos * Time.deltaTime * playerSpeed);
+	}
+
+}
