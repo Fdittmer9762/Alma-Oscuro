0fc811e2da3002a3ac035f25e3992b58645620ea
Fritz Dittmer <fdittmer9762@gmail.com>
2018-04-20 19:00:29 -0600

    Camera Follow
    Added Camera Follow Functionality
    **Camera Does Not rotate Around Player**
    Debugging: InProgress
    -Falling Issues
    -Camera rotation

---
+++ Unity/Alma Oscuro/Assets/Scripts/MonoBehaviours/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform CamTarget;
+
+	[Range(.01f,1f)]
+	public float smoothValue;
+
+	void Update () {
+		this.transform.position = Vector3.Lerp (this.transform.position, CamTarget.position, smoothValue);
+
+	}
+		
+}
