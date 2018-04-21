9fd3b99c55da22c7a524aada17b866742df8093d
Fritz Dittmer <fdittmer9762@gmail.com>
2018-04-20 18:23:02 -0600

    Jump/Gravity
    Added Jump and Gravity
    Debugging: Has an issue when transitioning from moving to falling, unknown caused

---
+++ Unity/Alma Oscuro/Assets/Scripts/MonoBehaviours/GroundedCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedCheck : MonoBehaviour {
+
+	private CharacterController cc;
+	private Animator anim;
+
+	void Start(){
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponentInChildren<Animator> ();
+	}
+
+	public void SetGrounded(){
+		anim.SetBool ("Grounded", cc.isGrounded);
+	}
+
+}
