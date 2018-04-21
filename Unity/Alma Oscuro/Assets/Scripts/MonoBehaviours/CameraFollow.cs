f5c1926d0d5a3228a7271d640e5e8e9987ed7073
Fritz Dittmer <fdittmer9762@gmail.com>
2018-04-20 19:40:04 -0600

    Camera Rotation
    Added camera rotation
    Needs fine tuning with values
    Debugged: Complete, no issues
    **still have issues with falling**

--- Unity/Alma Oscuro/Assets/Scripts/MonoBehaviours/CameraFollow.cs
+++ Unity/Alma Oscuro/Assets/Scripts/MonoBehaviours/CameraFollow.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform CamTarget;
+	private Vector3 tempRot;
+	private Vector3 currentRot;
 
 	[Range(.01f,1f)]
 	public float smoothValue;
 
 	void Update () {
 		this.transform.position = Vector3.Lerp (this.transform.position, CamTarget.position, smoothValue);
+		RotateCamera ();
+	}
 
+	void RotateCamera(){
+
+		currentRot = transform.rotation.eulerAngles;
+		currentRot.z = 0f;
+		transform.rotation = Quaternion.Euler (currentRot);
+
+		tempRot.y = -Input.GetAxis ("Mouse X");
+		tempRot.x = Input.GetAxis ("Mouse Y");
+		tempRot.z = 0;
+		transform.Rotate(tempRot*Time.deltaTime*25f);
 	}
 		
 }
